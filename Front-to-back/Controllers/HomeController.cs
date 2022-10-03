9daabe592d1339e30ac13c4fd6cdbc545a32277d
Orkhansha <orkhansha@code.edu.az>
2022-10-03 22:45:33 +0400

    Added front to back files

---
+++ Front-to-back/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Front_to_back.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {           
+            return View();
+        }
+    }
+}
